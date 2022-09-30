2f5d5f17a5c9eb4245b50062f0cc0c92ba9ee97f
Hesham <heshamelmasry1992@gmail.com>
2022-09-30 14:50:58 +0200

    use for loop and get user posts plus logout

--- day-5-start/1-get-all-posts-for-login-user/js/components/createHeaderBar.js
+++ day-5-start/1-get-all-posts-for-login-user/js/components/createHeaderBar.js
@@ -5,16 +5,24 @@ function createHeaderBar() {
     const navBar = document.querySelector("#nav-bar");
     if (navBar) {
         const userName = getUserName();
-        let navLinks = `
+        let navLinks;
+         navLinks = `
             <li class="p-8"><a href="/signup.html" class="${pathname === "/signup.html" ? "text-blue-600" : ""}">SignUp</a></li>
             <li class="p-8"><a href="/login.html" class="${pathname === "/login.html" ? "text-blue-600" : ""}">LogIn</a></li>
             `;
         if (userName) {
             navLinks = `
             <li class="p-8">
-                <a href="/create-post.html" class="${pathname === "/create-post.html" ? "text-blue-600" : ""}">Create Post</a>
+                <a href="/index.html" class="${pathname === "/index.html" ? "text-blue-600" : ""}">Home</a>
+            </li>
+            <li class="p-8">
+                <a href="/create-post.html" class="${pathname === "/create-post.html" ? "text-blue-600" : ""}">Create Post🎨</a>
+            </li>
+            <li class="p-8">
+                <a href="/my-posts.html" class="${pathname === "/my-posts.html" ? "text-blue-600" : ""}">My Posts💞</a>
             </li>
             <li class="p-8"><span>Hello 👋  ${userName}</span></li>
+            <li class="p-8"><button id="logout-btn">LogOut</button></li>
 `
         }
         navBar.innerHTML = `
